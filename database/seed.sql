ef162a097303a38b3e4426a697af63e49acc0237
Jusepez11 <jusepez90@gmail.com>
2026-03-13 03:03:04 -0400

    updated seed

--- database/seed.sql
+++ database/seed.sql
@@ -1,4 +1,4 @@
-INSERT INTO Tasks (device_id, task_description, due_date, is_complete) VALUES
+INSERT INTO task (device_id, task_description, due_date, is_complete) VALUES
     ('test-device-001', 'Buy groceries',         '2026-03-15 12:00:00', FALSE),
     ('test-device-001', 'Finish AWS setup',       NULL,                  FALSE),
     ('test-device-001', 'Walk the dog',           '2026-03-10 08:00:00', TRUE),
